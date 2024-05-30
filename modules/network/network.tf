7cc8465dd897ecce5ffc949b426003b0f382c56e
MatanP12 <matanp103@gmail.com>
2024-05-30 12:50:02 +0300

    Change ingress of sg

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "instance_sg" {
   }
   ingress {
     protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 5000
+    to_port     = 5000
     cidr_blocks = ["0.0.0.0/0"]
   }
 
